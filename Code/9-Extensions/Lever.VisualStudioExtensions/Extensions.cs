22c1e6bfff07d58c0c3562c4d200b59fb04e6ba9
CaiXiaoHui <jimcai@126.com>
2018-04-26 01:18:58 +0800

    add extensions todo

---
+++ Code/9-Extensions/Lever.VisualStudioExtensions/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meision.VisualStudio
+{
+    class Extensions
+    {
+        /*
+          <ItemGroup>
+    <Compile Update="NewFolder\Class1.cs;NewFolder\Class2.cs;NewFolder\Class3.cs">
+      <DesignTime>True</DesignTime>
+      <DependentUpon>Resource1.resx</DependentUpon>
+      <AutoGen>True</AutoGen>
+    </Compile>
+  </ItemGroup>*/
+    }
+}
